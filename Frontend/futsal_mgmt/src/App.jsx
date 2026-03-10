84390f436ecac48f77674739b35eb34b31af08bb
Sajan Limbu <limbusajan087@gmail.com>
2026-03-10 23:07:39 +0545

    files and folders initialized

---
+++ Frontend/futsal_mgmt/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div><h1>Futsal Management</h1></div>
+    </>
+  )
+}
+
+export default App
